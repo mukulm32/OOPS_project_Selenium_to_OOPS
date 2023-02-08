abb25a7f490465735c2df8e520798e2ea776c2fb
40032341 <mukulmangde3@gmail.com>
2023-02-08 10:52:10 +0530

    added pyetst

--- test_sleniumtooops.py
+++ test_sleniumtooops.py
@@ -1,5 +1,5 @@
-
-#project oops concept with selenium
+#import sleniumtooops as sl
+import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
@@ -31,20 +31,20 @@ class DetailsPage(LoginOpenCart):
             print("error! check for xpath")
 
 
+@pytest.fixture(scope='class')
+def setup_driver():
+    driver = webdriver.Chrome()
+    detail = DetailsPage(driver)
+    yield detail
+    driver.quit()
 
-driver = webdriver.Chrome()
-detail = DetailsPage(driver)
-while True:
-    print("Enter 1 for LOGIN ")
-    print("Enter 2 for VERIFY THE TEXT")
-    print("Enter 3 for CHECKING XPATH")
-    print("Enter 4 to Exit")
-    userchoice = int(input())
-    if userchoice == 1:
-        detail.login("mukulmangde3@gmail.com", "123456789")
-    elif userchoice == 2:
-        print(detail.get_details_text())
-    elif userchoice == 3:
-        detail.exp()
-    elif userchoice ==4:
-            quit()
+class TestLoginOpenCart:
+    def test_login(self, setup_driver):
+        setup_driver.login("mukulmangde3@gmail.com", "123456789")
+        actual_text = setup_driver.get_details_text()
+        assert actual_text == 'Account Details', f"Expected text is 'Account Details' but got '{actual_text}'"
+
+    def test_xpath_check(self, setup_driver):
+        setup_driver.exp()
+        assert setup_driver.driver.find_element(By.LINK_TEXT,"LOGIN").text == "LOGIN", "Could not find LOGIN link"
+
